0d229120c9b7074a595840b06893b8b92f29cdb7
bhupeshbhatia <bhatiabh@sheridancollege.ca>
2018-11-01 02:35:30 -0400

    Initial Commit

---
+++ test/metrics_suite_test.go
@@ -0,0 +1,187 @@
+package test
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/Shopify/sarama"
+	"github.com/TerrexTech/agg-metrics-cmd/metric"
+	"github.com/TerrexTech/go-commonutils/commonutil"
+	"github.com/TerrexTech/go-eventstore-models/model"
+	"github.com/TerrexTech/go-kafkautils/kafka"
+	"github.com/TerrexTech/uuuid"
+	"github.com/joho/godotenv"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/pkg/errors"
+)
+
+func Byf(s string, args ...interface{}) {
+	By(fmt.Sprintf(s, args...))
+}
+
+func TestMetrics(t *testing.T) {
+	log.Println("Reading environment file")
+	err := godotenv.Load("../.env")
+	if err != nil {
+		err = errors.Wrap(err,
+			".env file not found, env-vars will be read as set in environment",
+		)
+		log.Println(err)
+	}
+
+	missingVar, err := commonutil.ValidateEnv(
+		"KAFKA_BROKERS",
+		"KAFKA_CONSUMER_EVENT_GROUP",
+
+		"KAFKA_CONSUMER_EVENT_TOPIC",
+		"KAFKA_CONSUMER_EVENT_QUERY_GROUP",
+		"KAFKA_CONSUMER_EVENT_QUERY_TOPIC",
+
+		"KAFKA_PRODUCER_EVENT_TOPIC",
+		"KAFKA_PRODUCER_EVENT_QUERY_TOPIC",
+		"KAFKA_PRODUCER_RESPONSE_TOPIC",
+
+		"MONGO_HOSTS",
+		"MONGO_USERNAME",
+		"MONGO_PASSWORD",
+		"MONGO_DATABASE",
+		"MONGO_CONNECTION_TIMEOUT_MS",
+		"MONGO_RESOURCE_TIMEOUT_MS",
+	)
+
+	if err != nil {
+		err = errors.Wrapf(err, "Env-var %s is required for testing, but is not set", missingVar)
+		log.Fatalln(err)
+	}
+
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "MetricAggregate Suite")
+}
+
+var _ = Describe("MetricAggregate", func() {
+	var (
+		kafkaBrokers          []string
+		eventsTopic           string
+		producerResponseTopic string
+
+		mockMetric *metric.Metric
+		mockEvent  *model.Event
+	)
+	BeforeSuite(func() {
+		kafkaBrokers = *commonutil.ParseHosts(
+			os.Getenv("KAFKA_BROKERS"),
+		)
+		eventsTopic = os.Getenv("KAFKA_PRODUCER_EVENT_TOPIC")
+		producerResponseTopic = os.Getenv("KAFKA_PRODUCER_RESPONSE_TOPIC")
+
+		metricID, err := uuuid.NewV4()
+		Expect(err).ToNot(HaveOccurred())
+
+		deviceID, err := uuuid.NewV4()
+		Expect(err).ToNot(HaveOccurred())
+
+		itemID, err := uuuid.NewV4()
+		Expect(err).ToNot(HaveOccurred())
+
+		timestamp := time.Now().Unix()
+		Expect(err).ToNot(HaveOccurred())
+
+		mockMetric = &metric.Metric{
+			MetricID:      metricID,
+			ItemID:        itemID,
+			DeviceID:      deviceID,
+			Timestamp:     timestamp,
+			TempIn:        23.5,
+			Humidity:      45,
+			Ethylene:      50,
+			CarbonDioxide: 400,
+		}
+
+		log.Println(mockMetric.DeviceID)
+		marshalMetric, err := json.Marshal(mockMetric)
+		Expect(err).ToNot(HaveOccurred())
+
+		cid, err := uuuid.NewV4()
+		Expect(err).ToNot(HaveOccurred())
+		uid, err := uuuid.NewV4()
+		Expect(err).ToNot(HaveOccurred())
+		timeUUID, err := uuuid.NewV1()
+		Expect(err).ToNot(HaveOccurred())
+		mockEvent = &model.Event{
+			Action:        "insert",
+			CorrelationID: cid,
+			AggregateID:   metric.AggregateID,
+			Data:          marshalMetric,
+			Timestamp:     time.Now(),
+			UserUUID:      uid,
+			TimeUUID:      timeUUID,
+			Version:       0,
+			YearBucket:    2018,
+		}
+	})
+
+	Describe("Metrics Operations", func() {
+		It("should insert record", func(done Done) {
+			Byf("Producing MockEvent")
+			p, err := kafka.NewProducer(&kafka.ProducerConfig{
+				KafkaBrokers: kafkaBrokers,
+			})
+			Expect(err).ToNot(HaveOccurred())
+			marshalEvent, err := json.Marshal(mockEvent)
+			Expect(err).ToNot(HaveOccurred())
+			p.Input() <- kafka.CreateMessage(eventsTopic, marshalEvent)
+
+			// Check if MockEvent was processed correctly
+			Byf("Consuming Result")
+			c, err := kafka.NewConsumer(&kafka.ConsumerConfig{
+				KafkaBrokers: kafkaBrokers,
+				GroupName:    "aggMetric.test.group.1",
+				Topics:       []string{producerResponseTopic},
+			})
+			msgCallback := func(msg *sarama.ConsumerMessage) bool {
+				defer GinkgoRecover()
+				kr := &model.KafkaResponse{}
+				err := json.Unmarshal(msg.Value, kr)
+				Expect(err).ToNot(HaveOccurred())
+
+				if kr.UUID == mockEvent.TimeUUID {
+					Expect(kr.Error).To(BeEmpty())
+					Expect(kr.ErrorCode).To(BeZero())
+					Expect(kr.CorrelationID).To(Equal(mockEvent.CorrelationID))
+					Expect(kr.UUID).To(Equal(mockEvent.TimeUUID))
+
+					metric := &metric.Metric{}
+					err = json.Unmarshal(kr.Result, metric)
+					Expect(err).ToNot(HaveOccurred())
+
+					if metric.ItemID == mockMetric.ItemID {
+						mockMetric.ID = metric.ID
+						Expect(metric).To(Equal(mockMetric))
+						return true
+					}
+				}
+				return false
+			}
+
+			handler := &msgHandler{msgCallback}
+			c.Consume(context.Background(), handler)
+
+			Byf("Checking if record got inserted into Database")
+			aggColl, err := loadAggCollection()
+			Expect(err).ToNot(HaveOccurred())
+			findResult, err := aggColl.FindOne(mockMetric)
+			Expect(err).ToNot(HaveOccurred())
+			findMetric, assertOK := findResult.(*metric.Metric)
+			Expect(assertOK).To(BeTrue())
+			Expect(findMetric).To(Equal(mockMetric))
+
+			close(done)
+		}, 20)
+	})
+})
